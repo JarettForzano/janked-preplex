b76ee40081ce23ac381e2df6a3f62bfc01b3ed28
Jarett Forzano <jarett@Jaretts-MacBook-Air.local>
2024-10-11 23:10:45 -0700

    got display working sorta

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import SearchEngine from './components/search';
+import SearchEngine from './components/search.tsx';
 import Display from './components/display.tsx';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<SearchEngine />} />
+        <Route path="/search" element={<SearchEngine />} />
         <Route path="/search/:query" element={<Display />} />
       </Routes>
     </Router>
